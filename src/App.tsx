1e9d4ee5f27fb0062d06d1d4f59e25eba12ca8e8
mosiyo <mkermani1383@gmail.com>
2026-01-03 01:58:40 +0330

    Add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter basename="/mostafa-s-digital-canvas">        <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
